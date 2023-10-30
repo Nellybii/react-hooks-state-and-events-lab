17dbd5f3c57248f88cecc297e51e820126684621
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-10-30 03:49:47 +0300

    onChange event

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(" ");
+  const handleClick = () => {
+    setIsInCart();
+  };
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>
+        {isInCart ? "Add to Cart" : "Remove from Cart"}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  // Initialize the selectedCategory state variable with a default value
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  // Create a filteredItems array to store items based on the selected category
+  const filteredItems = selectedCategory === "All"
+    ? items
+    : items.filter(item => item.category === selectedCategory);
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        {/* Add an onChange event handler to update selectedCategory */}
+        <select name="filter" onChange={(e) => setSelectedCategory(e.target.value)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +22,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
